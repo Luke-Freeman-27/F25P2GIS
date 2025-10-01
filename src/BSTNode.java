af4a7a977875d91bae0c687807047a0346f372ae
walkerberndt <124514323+walkerberndt@users.noreply.github.com>
2025-10-01 13:25:42 -0400

    BST Start

---
+++ src/BSTNode.java
@@ -0,0 +1,17 @@
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author walkerberndt
+ *  @version Oct 1, 2025
+ */
+public class BSTNode {
+    //~ Fields ................................................................
+    
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
