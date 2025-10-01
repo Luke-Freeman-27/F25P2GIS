4bd0a184515284fe7fab06c90b006e6be287caec
Luke-Freeman-27 <lukef23@vt.edu>
2025-10-01 12:57:20 -0400

    I'm stuff

---
+++ src/City.java
@@ -0,0 +1,15 @@
+/**
+ * City class for the implementation of a BST tree.
+ * 
+ * @author walkerberndt, Luke-Freeman-27
+ * @version Oct 1, 2025
+ */
+
+public class City {
+    // ~ Fields ................................................................
+    /**
+     * @param Name is the given name for the city
+     * @param x is the x-cordinate value for the locaiton of the city
+     * @param
+     */
+}
